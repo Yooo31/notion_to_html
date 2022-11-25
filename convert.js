9201beddb4a239208172962a2363339b925ca478
Yooo31 <mrts.yoan@gmail.com>
2022-11-25 11:14:16 +0100

    Function to convert each sentence

--- convert.js
+++ convert.js
@@ -1,14 +1,15 @@
-var originalText, HTMLlist, result, breakOriginalText, lineBuild, rowCount, tab;
+var originalText, HTMLlist, breakOriginalText, lineBuild, rowCount, tab;
 
 function getresult() {
-    tab = [];
-    originalText = document.getElementById('originalContent').value;
-    result = "[";
-    breakOriginalText = originalText.split("\n");
-    rowCount = breakOriginalText.length;
+  tab = [];
+  originalText = document.getElementById('originalContent').value;
+  breakOriginalText = originalText.split("\n");
+  rowCount = breakOriginalText.length;
 
-    for (var r = 0; r < rowCount; r++) {
-      lineBuild = 0 === r ? breakOriginalText[r] : breakOriginalText[r];
-      tab.push(lineBuild);
-    }
+  for (var r = 0; r < rowCount; r++) {
+    lineBuild = 0 === r ? breakOriginalText[r] : breakOriginalText[r];
+    tab.push(lineBuild);
+  }
+
+  convertText(tab);
 }
